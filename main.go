2dd560cda1b016ffcc0113a0561acc2afec1b419
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-18 00:16:13 +0200

    Add a -version flag

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"gopkg.in/edn.v1"
 )
 
+const (
+	Version = "0.1.1"
+)
+
 func main() {
+	var printVersion bool
+
+	flag.BoolVar(&printVersion, "version", false, "print the version and exit")
 	flag.Parse()
+
+	if printVersion {
+		fmt.Printf("Epp version %s\n", Version)
+		return
+	}
 
 	input := bufio.NewReader(os.Stdin)
 	output := bufio.NewWriter(os.Stdout)
