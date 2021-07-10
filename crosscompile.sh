92f892190b59af48437bdbc6bd3fd85672d21a42
Baptiste Fontaine <b@ptistefontaine.fr>
2021-07-10 13:41:04 +0200

    strip binaries

--- crosscompile.sh
+++ crosscompile.sh
@@ -3,7 +3,9 @@ for os in linux darwin windows; do
   for arch in amd64 386; do
     if [[ "$os-$arch" = "darwin-386" ]]; then continue; fi
     name="epp-$os-$arch"
-    env GOOS=$os GOARCH=$arch go build -o $name .
+    # -s: Omit the symbol table and debug information.
+    # -w: Omit the DWARF symbol table.
+    env GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o $name .
     shasum -a 256 $name
   done
 done
